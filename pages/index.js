a6c214468014e9a89e08f176cdd49373601f0cc6
anathan <anathan.gv@gmail.com>
2021-11-17 20:31:59 -0300

    inicio do alurakut

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,53 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+import { Fragment } from 'react';
+import { MainGrid } from './src/componets/MainGrid/index';
+import { Box } from './src/componets/Box';
+import { AlurakutMenu, OrkutNostalgicIconSet } from './src/lib/alurakutCommuns'
+import { ProfileRelationsBoxWrapper } from './src/componets/ProfileRelations/index'
 
 export default function Home() {
-  return <Title>My page</Title>
+
+  const githubUser = 'anathan';
+  const pesssoasFavoritas = ['peas','anathan','rennyer','mario','jhon','rocketseat'];
+
+  return (
+    <Fragment>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{gridArea: 'profileArea'}}>
+          <Box>
+            <img src={`https://github.com/${githubUser}.png`} style={{borderRadius: '8px'}}/>
+          </Box>
+        </div>
+        <div className="welcomeArea" style={{gridArea: 'welcomeArea'}}>
+          <Box> 
+            <h1 className="title">
+              Bem vindo(a)
+            </h1>
+
+            <OrkutNostalgicIconSet />
+          </Box>
+        </div>
+        <div className="profileRelationsArea" style={{gridArea: 'profileRelationsArea'}}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Pessoas da comunidade ({pesssoasFavoritas.length})
+            </h2>
+            <ul>
+              {pesssoasFavoritas.map((itenAtual) => {
+                return (
+                  <li>
+                    <a href={`/users/${itenAtual}`} key={itenAtual}>
+                      <img src={`https://github.com/${itenAtual}.png`}/>
+                      <span>{itenAtual}</span>
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </ProfileRelationsBoxWrapper>
+          <Box>Comunidades</Box>
+        </div>
+      </MainGrid>
+    </Fragment>
+  )
 }
